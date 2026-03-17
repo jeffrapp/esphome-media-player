5b0c08bb506bb39e3ffe18c53780427122882f1e
jtenniswood <james@tenniswood.co.uk>
2026-03-17 22:18:27 +0000

    Add Buy Me A Coffee widget to VitePress config for user support
    Integrated a "Buy Me A Coffee" widget into the VitePress configuration to encourage user contributions and enhance community support for the project. This addition aims to improve visibility and accessibility for donations.

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,6 +3,23 @@ export default {
   title: 'ESPHome Media Player',
   description: 'A media controller for Home Assistant',
   ignoreDeadLinks: true,
+  head: [
+    [
+      'script',
+      {
+        'data-name': 'BMC-Widget',
+        'data-cfasync': 'false',
+        src: 'https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js',
+        'data-id': 'jtenniswood',
+        'data-description': 'Support me on Buy me a coffee!',
+        'data-message': '',
+        'data-color': '#FFDD00',
+        'data-position': 'Right',
+        'data-x_margin': '18',
+        'data-y_margin': '18',
+      },
+    ],
+  ],
   themeConfig: {
     socialLinks: [
       { icon: 'github', link: 'https://github.com/jtenniswood/esphome-media-player' },
